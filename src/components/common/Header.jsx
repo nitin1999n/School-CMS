937776dee475ef098d299422623e02b147b9fe5a
Nitin Sharma <nitin2019it035abesit@gmail.com>
2025-03-04 08:45:14 +0530

    first commit

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
 import "./style.css";
 
-const HeaderBar = () => {
+const HeaderBar = ({ onToggle }) => {
   return (
     <div className="topnav">
       <Navbar
@@ -12,7 +13,26 @@ const HeaderBar = () => {
         variant="dark"
         className="topnav"
       >
-        <Navbar.Brand href="" className="mx-auto">HRMS</Navbar.Brand>
+        {/* <Nav className="mr-auto d-flex align-items-center">
+          <NavItem onClick={onToggle}>
+            <NavIcon>
+              <i className="bi bi-list" style={{ fontSize: "1.5em", color: "white", marginRight: "10px" }}></i>
+            </NavIcon>
+          </NavItem>
+        </Nav> */}
+        <Navbar.Brand href="" className="mx-auto">CMS</Navbar.Brand>
+        <Nav className="ml-auto d-flex align-items-center">
+          <NavItem>
+            <NavIcon>
+              <i className="bi bi-bell" style={{ fontSize: "1.5em", color: "white", marginRight: "10px" }}></i>
+            </NavIcon>
+          </NavItem>
+          <NavItem>
+            <NavIcon>
+              <i className="bi bi-gear" style={{ fontSize: "1.5em", color: "white" }}></i>
+            </NavIcon>
+          </NavItem>
+        </Nav>
       </Navbar>
     </div>
   );
